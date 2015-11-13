b2683540b01f45bb5623b395420931f762b2a464
Mathieu <mathieu.andreux@gmail.com>
2015-11-13 10:57:33 +0100

    added whitespace around operator !=

--- exercises/070/solution.py
+++ exercises/070/solution.py
@@ -7,5 +7,5 @@ Created on Fri Oct 02 10:01:49 2015
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 for i in range(len(alphabet)):
     for j in range(len(alphabet)):
-        if(i!=j):
+        if(i != j):
             print(alphabet[i] + alphabet[j])
